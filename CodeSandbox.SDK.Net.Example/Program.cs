4be204d7c2cb9f50b8475e93fad5ec0e6db53ffe
E33orNaut <errornaut25@gmail.com>
2025-05-21 23:35:45 +0100

    Completed examples for all the calls present right now
    Some other minor tweeks

--- CodeSandbox.SDK.Net.Example/Program.cs
+++ CodeSandbox.SDK.Net.Example/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using CodeSandbox.SDK.Models;
 using CodeSandbox.SDK.Net;
@@ -9,63 +7,58 @@ using CodeSandbox.SDK.Net.Internal;
 using CodeSandbox.SDK.Net.Models;
 using CodeSandbox.SDK.Net.Services;
 using CodeSandbox.SDK.Services;
+
 namespace CodeSandbox.SDK.Net.Example
 {
     internal class Program
     {
         static async Task Main()
         {
-            // nuget install CodeSandbox.SDK.Net
-            // dotnet add package CodeSandbox.SDK.Net --version 0.0.11
-
-            // Set your codesandbox.io api key here
             var client = new ApiClient("api-token");
 
+            // ContainerService example
+            var containerService = new ContainerService(client);
+            var containerRequest = new ContainerSetupRequest { TemplateId = "template-id" };
+            var containerResponse = await containerService.SetupContainerAsync(containerRequest);
+            Console.WriteLine($"Container setup result: {containerResponse.Result}");
 
+            // GitService examples
+            var gitService = new GitService(client);
+            await gitService.PostCommitAsync("Initial commit");
+            var gitStatus = await gitService.GetStatusAsync();
+            Console.WriteLine($"Git status: {gitStatus.Status}"); 
+            await gitService.PostPullAsync("main");
+            await gitService.PostDiscardAsync(new[] { "file.txt" });
+            await gitService.PostRemoteAddAsync("https://github.com/user/repo.git");
 
-            // CREATE CONTAINER EXAMPLE
-            // Configure the containerSetupRequest with the template ID and features
-            ContainerSetupRequest request = new ContainerSetupRequest
-            {
-                TemplateId = "something-i-dont-actually-know"
-            };
-            _ = new List<ContainerSetupFeature>
-            {
-                new ContainerSetupFeature()
-            };
+            // PortService example
+            var portService = new PortService(client);
+            var portList = await portService.GetPortListAsync();
+            foreach (var port in portList.Result.List)
+                Console.WriteLine($"Port: {port.PortNumber}, Url: {port.Url}");
 
-            // Execut the container setup request and get the result
-            var files = await new ContainerService(client).SetupContainerAsync(request);
+            // SandboxFsService examples
+            var fsService = new SandboxFsService(client);
+            await fsService.WriteFileAsync(new WriteFileRequest { Path = "file.txt", Content = "Hello World" });
+            await fsService.FsReadFileAsync(new FSReadFileParams { Path = "file.txt" });
+            await fsService.FsUploadAsync(new UploadRequest { ParentId = "root", Content = "data" });
+            await fsService.FsDownloadAsync(new DownloadRequest { Path = "file.txt" });
+            await fsService.FsPathSearchAsync(new PathSearchParams());
+            await fsService.StatAsync(new FSStatParams { Path = "file.txt" });
+            await fsService.CopyAsync(new FSCopyParams { SourcePath = "file.txt" });
+            await fsService.RenameAsync(new FSRenameParams { /* fill as needed */ });
+            await fsService.RemoveAsync(new FSRemoveParams { Path = "file.txt" });
+            await fsService.ReadDirAsync(new FSReadDirParams { Path = "." });
 
-            // Printe the result.
-            Console.WriteLine(files.Result);
-
-
-            // PUSH COMMIT EXAMPLE
-            await new GitService(client).PostCommitAsync("Commit message");
-
-            // GIT STATUS DIFF STATUS
-            await new GitService(client).GetTargetDiffAsync("branch");
-
-            // GET PORT LIST EXAMPLE
-            var ports = await new PortService(client).GetPortListAsync(cancellationToken: default);
-            foreach (var x in ports.Result.List)
-            {
-                Console.WriteLine($"Port: {x.PortNumber}, Url: {x.Url} ");
-            }
-
-            // WRITE A FILE IN SANDBOX
-            WriteFileRequest writeRequest = new WriteFileRequest
-            {
-                Content = "Hello World!",
-                Path = "path/to/file.txt",
-            };
-            await new SandboxFsService(client).WriteFileAsync(writeRequest);
-
-            // INITALIZE SETUP EXAMPLE
-            await new SetupService(client).InitializeSetupAsync();
-
-
+            // SetupService examples
+            var setupService = new SetupService(client);
+            await setupService.InitializeSetupAsync();
+            await setupService.GetSetupProgressAsync();
+            await setupService.SkipStepAsync(0);
+            await setupService.SkipAllStepsAsync();
+            await setupService.EnableSetupAsync();
+            await setupService.DisableSetupAsync();
+            await setupService.SetStepAsync(0);
         }
     }
 }
