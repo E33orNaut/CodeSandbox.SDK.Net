624c599cd8c4f644d22e10660ccd3c54695c5430
E33orNaut <errornaut25@gmail.com>
2025-05-27 13:07:14 +0100

    Sync

---
+++ CodeSandbox.SDK.Net.Sockets/Hubs/SetupHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSandbox.SDK.Net.Internal;
+using CodeSandbox.SDK.Net.Services;
+using Microsoft.AspNet.SignalR;
+
+namespace CodeSandbox.SDK.Net.Sockets.Hubs
+{
+    public class SetupHub : Hub
+    {
+
+        private static ApiClient client = new ApiClient(ServerContext.ApiKey);
+        private static SetupService service = new SetupService(client);
+
+
+
+    }
+}
